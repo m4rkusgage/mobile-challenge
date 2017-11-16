d79373fb6f88a491c76d69d9c71d1dd9cbb3bb4a
Markus Gage <markgage86@gmail.com>
2017-11-16 15:40:50 -0500

    setup constants for oauth

---
+++ 500pxChallenge/500pxChallenge/500px-API/MGConstants.h
@@ -0,0 +1,22 @@
+//
+//  MGConstants.h
+//  500px-iOS
+//
+//  Created by Markus Gage on 2017-11-16.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MGConstants : NSObject
+
+extern NSString *const kMG500pxClientKey;
+extern NSString *const kMG500pxClientSecret;
+extern NSString *const kMG500pxCallBackURL;
+
+extern NSString *const kMG500pxAPIBase;
+extern NSString *const kMG500pxAPIRequestTokenPath;
+extern NSString *const kMG500pxAPIAuthorizePath;
+extern NSString *const kMG500pxAPIAccessTokenPath;
+
+@end
---
+++ 500pxChallenge/500pxChallenge/500px-API/MGConstants.m
@@ -0,0 +1,22 @@
+//
+//  MGConstants.m
+//  500px-iOS
+//
+//  Created by Markus Gage on 2017-11-16.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import "MGConstants.h"
+
+@implementation MGConstants
+
+NSString *const kMG500pxClientKey = @"kGTi5iNcUvzUg7OvAadS11BvYOnAOmw9cX7iyuMC";
+NSString *const kMG500pxClientSecret = @"xix4PYGx2vvGuIUzLh33vdt7qbfmNtqZ1F0EdDBr";
+NSString *const kMG500pxCallBackURL = @"com.mark.500px://";
+
+NSString *const kMG500pxAPIBase = @"https://api.500px.com/v1";
+NSString *const kMG500pxAPIRequestTokenPath = @"/oauth/request_token";
+NSString *const kMG500pxAPIAuthorizePath = @"/oauth/authorize";
+NSString *const kMG500pxAPIAccessTokenPath = @"/oauth/access_token";
+
+@end
