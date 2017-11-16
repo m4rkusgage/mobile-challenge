f7cb8cd1e599a36067ca6b517d75f240a6383305
Markus Gage <markgage86@gmail.com>
2017-11-16 14:50:04 -0500

    initial project setup

---
+++ 500pxChallenge/500pxChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  500pxChallenge
+//
+//  Created by Markus Gage on 2017-11-16.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 500pxChallenge/500pxChallenge/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  500pxChallenge
+//
+//  Created by Markus Gage on 2017-11-16.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
