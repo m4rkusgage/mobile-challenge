71192d0e98e5776081660f668d5e26b841c915ba
Markus Gage <markgage86@gmail.com>
2017-11-16 19:03:09 -0500

    added constants for photo api parameters

--- 500pxChallenge/500pxChallenge/500px-API/MGConstants.h
+++ 500pxChallenge/500pxChallenge/500px-API/MGConstants.h
@@ -19,4 +19,30 @@ extern NSString *const kMG500pxAPIRequestTokenPath;
 extern NSString *const kMG500pxAPIAuthorizePath;
 extern NSString *const kMG500pxAPIAccessTokenPath;
 
+extern NSString *const kMG500pxAPIPhotosPath;
+
+extern NSString *const kMG500pxPhotoParameterFeature;
+extern NSString *const kMG500pxPhotoParameterOnly;
+extern NSString *const kMG500pxPhotoParameterExclude;
+extern NSString *const kMG500pxPhotoParameterSort;
+extern NSString *const kMG500pxPhotoParameterSortDirection;
+extern NSString *const kMG500pxPhotoParameterPage;
+extern NSString *const kMG500pxPhotoParameterImageSize;
+extern NSString *const kMG500pxPhotoParameterTags;
+
+extern NSString *const kMG500pxPhotoFeaturePopular;
+
+extern NSString *const kMG500pxPhotoCategoryNude;
+extern NSString *const kMG500pxPhotoCategoryTravel;
+extern NSString *const kMG500pxPhotoCategoryLandscape;
+
+extern NSString *const kMG500pxPhotoSortCreated;
+extern NSString *const kMG500pxPhotoSortRating;
+extern NSString *const kMG500pxPhotoSortTaken;
+
+extern NSString *const kMG500pxPhotoSortDirectionAsc;
+extern NSString *const kMG500pxPhotoSortDirectionDesc;
+
+extern NSString *const kMG500pxPhotoImageSize4;
+
 @end
--- 500pxChallenge/500pxChallenge/500px-API/MGConstants.m
+++ 500pxChallenge/500pxChallenge/500px-API/MGConstants.m
@@ -19,4 +19,30 @@ NSString *const kMG500pxAPIRequestTokenPath = @"/oauth/request_token";
 NSString *const kMG500pxAPIAuthorizePath = @"/oauth/authorize";
 NSString *const kMG500pxAPIAccessTokenPath = @"/oauth/access_token";
 
+NSString *const kMG500pxAPIPhotosPath = @"/photos";
+
+NSString *const kMG500pxPhotoParameterFeature = @"feature";
+NSString *const kMG500pxPhotoParameterOnly = @"only";
+NSString *const kMG500pxPhotoParameterExclude = @"exclude";
+NSString *const kMG500pxPhotoParameterSort = @"sort";
+NSString *const kMG500pxPhotoParameterSortDirection = @"sort_direction";
+NSString *const kMG500pxPhotoParameterPage = @"page";
+NSString *const kMG500pxPhotoParameterImageSize = @"image_size";
+NSString *const kMG500pxPhotoParameterTags = @"tags";
+
+NSString *const kMG500pxPhotoFeaturePopular = @"popular";
+
+NSString *const kMG500pxPhotoCategoryNude = @"Nude";
+NSString *const kMG500pxPhotoCategoryTravel = @"Travel";
+NSString *const kMG500pxPhotoCategoryLandscape = @"Landscape";
+
+NSString *const kMG500pxPhotoSortCreated = @"created_at";
+NSString *const kMG500pxPhotoSortRating = @"rating";
+NSString *const kMG500pxPhotoSortTaken = @"taken_at";
+
+NSString *const kMG500pxPhotoSortDirectionAsc = @"asc";
+NSString *const kMG500pxPhotoSortDirectionDesc = @"desc";
+
+NSString *const kMG500pxPhotoImageSize4 = @"4";
+
 @end
