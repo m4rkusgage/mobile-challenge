be73f488a9f73085350f94f54b0cd74d5591af7c
Markus Gage <markgage86@gmail.com>
2017-11-18 17:32:21 -0500

    created a fullscreen gallery layout

---
+++ 500pxChallenge/500pxChallenge/Gallery Fullscreen/MGFullscreenLayout.h
@@ -0,0 +1,13 @@
+//
+//  MGFullscreenLayout.h
+//  500px-iOS
+//
+//  Created by Markus Gage on 2017-11-18.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGFullscreenLayout : UICollectionViewLayout
+
+@end
---
+++ 500pxChallenge/500pxChallenge/Gallery Fullscreen/MGFullscreenLayout.m
@@ -0,0 +1,79 @@
+//
+//  MGFullscreenLayout.m
+//  500px-iOS
+//
+//  Created by Markus Gage on 2017-11-18.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import "MGFullscreenLayout.h"
+
+@interface MGFullscreenLayout ()
+@property (assign, nonatomic) CGFloat contentWidth;
+@property (strong, nonatomic) NSMutableArray<UICollectionViewLayoutAttributes *> *attributeCache;
+@end
+
+@implementation MGFullscreenLayout
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self layoutSetup];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self layoutSetup];
+    }
+    return self;
+}
+
+- (void)layoutSetup {
+    self.contentWidth = 0;
+    self.attributeCache = [[NSMutableArray alloc] init];
+}
+
+- (CGSize)collectionViewContentSize {
+    return CGSizeMake(self.contentWidth, self.collectionView.bounds.size.height);
+}
+
+- (void)prepareLayout {
+    [super prepareLayout];
+    
+    CGFloat xOffset = 0;
+    CGFloat yOffset = 0;
+    
+    for (int item = 0; item < [self.collectionView numberOfItemsInSection:0]; item++) {
+        NSIndexPath *indexPath = [NSIndexPath indexPathForItem:item inSection:0];
+        CGSize itemSize = self.collectionView.bounds.size;
+        
+        CGRect frame = CGRectMake(xOffset, yOffset, itemSize.width, itemSize.height);
+        
+        UICollectionViewLayoutAttributes *attributes = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+        attributes.frame = frame;
+        
+        [self.attributeCache addObject:attributes];
+        
+        self.contentWidth = frame.origin.x + frame.size.width;
+        
+        xOffset += itemSize.width;
+    }
+}
+
+- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSMutableArray *layoutAttributes = [[NSMutableArray alloc] init];
+    for (UICollectionViewLayoutAttributes *attributes in self.attributeCache) {
+        if (CGRectIntersectsRect(attributes.frame, rect)) {
+            [layoutAttributes addObject:attributes];
+        }
+    }
+    return layoutAttributes;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return self.attributeCache[indexPath.item];
+}
+@end
