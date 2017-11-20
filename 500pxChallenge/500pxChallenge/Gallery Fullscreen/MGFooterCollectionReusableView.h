d553791820e4d2d969a802d7b5e31a2ffc8ad2a2
Markus Gage <markgage86@gmail.com>
2017-11-20 17:28:00 -0500

    basic custom footer for collectionview

---
+++ 500pxChallenge/500pxChallenge/Gallery Fullscreen/MGFooterCollectionReusableView.h
@@ -0,0 +1,13 @@
+//
+//  MGFooterCollectionReusableView.h
+//  500px-iOS
+//
+//  Created by Markus Gage on 2017-11-20.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGFooterCollectionReusableView : UICollectionReusableView
+
+@end
---
+++ 500pxChallenge/500pxChallenge/Gallery Fullscreen/MGFooterCollectionReusableView.m
@@ -0,0 +1,18 @@
+//
+//  MGFooterCollectionReusableView.m
+//  500px-iOS
+//
+//  Created by Markus Gage on 2017-11-20.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import "MGFooterCollectionReusableView.h"
+
+@implementation MGFooterCollectionReusableView
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
