693111d841cd0267fb914dea6c8f3edd2de2b8f7
Markus Gage <markgage86@gmail.com>
2017-11-25 13:05:31 -0500

    refactored view controller

--- 500pxChallenge/500pxChallenge/Gallery Grid/MGGridCollectionViewController.m
+++ 500pxChallenge/500pxChallenge/Gallery Grid/MGGridCollectionViewController.m
@@ -7,26 +7,38 @@
 //
 
 #import "MGGridCollectionViewController.h"
-#import "MGGridLayout.h"
-#import "MGGridCollectionViewCell.h"
-#import "MGApiClient.h"
 #import "MGGalleryFullscreenCollectionViewController.h"
+#import "MGApiClient.h"
+#import "MGGridCollectionViewCell.h"
+#import "MGGridLayout.h"
 #import "MGLayoutDelegate.h"
 #import "MGViewControllerDelegate.h"
 #import "MGTransitionAnimator.h"
+#import "NSArray+MGUtilities.h"
 
 @interface MGGridCollectionViewController ()<MGLayoutDelegate, UINavigationControllerDelegate, MGViewControllerDelegate>
 @property (assign, nonatomic) NSInteger pageNumer;
 @property (strong, nonatomic) NSMutableArray *photoArray;
-@property (strong, nonatomic) MGApiClient *apiClient;
-@property (strong, nonatomic) NSIndexPath *currentSelectedIndex;
 @property (strong, nonatomic) NSIndexPath *currentIndex;
+@property (strong, nonatomic) MGApiClient *apiClient;
 @end
 
 @implementation MGGridCollectionViewController
 
 static NSString * const reuseIdentifier = @"GridCell";
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.navigationController setDelegate:self];
+    [self.navigationController setNavigationBarHidden:YES];
+    
+    [self.collectionView registerNib:[UINib nibWithNibName:@"MGGridCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:reuseIdentifier];
+    
+    [self downloadUpdateContent];
+}
+
+#pragma mark - Getter Methods
 - (MGApiClient *)apiClient {
     if (!_apiClient) {
         _apiClient = [MGApiClient sharedInstance];
@@ -41,40 +53,28 @@ static NSString * const reuseIdentifier = @"GridCell";
     return _photoArray;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    [self.navigationController setDelegate:self];
-    [self.navigationController setNavigationBarHidden:YES];
-    
-    self.pageNumer = 1;
-    
-    [self.collectionView registerNib:[UINib nibWithNibName:@"MGGridCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:reuseIdentifier];
-    
-    [self.apiClient getListPhotosForFeature:kMG500pxPhotoFeaturePopular
-                         includedCategories:@[]
-                         excludedCategories:@[]
-                                       page:self.pageNumer
-                                 completion:^(NSArray *result, NSError *error) {
-                                     self.photoArray = [result mutableCopy];
-                                     [self.collectionView reloadData];
-                                 }];
+- (NSIndexPath *)currentIndex {
+    if (!_currentIndex) {
+        NSArray *indexPaths = [self.collectionView indexPathsForVisibleItems];
+        indexPaths = [indexPaths sortedArrayByKey:@"item" ascending:YES];
+        NSInteger midPoint = [indexPaths count] / 2;
+        return indexPaths[midPoint];
+    }
+    return _currentIndex;
 }
 
-#pragma mark <UICollectionViewDataSource>
-
+#pragma mark - UICollectionViewDataSource
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     return 1;
 }
-
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     return [self.photoArray count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    self.currentIndex = indexPath;
-    MGPhoto *photo = self.photoArray[indexPath.item];
     
+    MGPhoto *photo = self.photoArray[indexPath.item];
     MGGridCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
     if (!photo.wasShown) {
         [cell reset];
@@ -89,26 +89,19 @@ static NSString * const reuseIdentifier = @"GridCell";
     return cell;
 }
 
+#pragma mark - UICollectionViewDelegate
 - (void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.item == [self.photoArray count] - 5) {
-        self.pageNumer += 1;
-        [self.apiClient getListPhotosForFeature:kMG500pxPhotoFeaturePopular
-                             includedCategories:@[]
-                             excludedCategories:@[]
-                                           page:self.pageNumer
-                                     completion:^(NSArray *result, NSError *error) {
-                                         [self.photoArray addObjectsFromArray:result];
-                                         [self.collectionView reloadData];
-                                     }];
+        [self downloadUpdateContent];
     }
 }
 
-#pragma mark <UICollectionViewDelegate>
--(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    self.currentSelectedIndex = indexPath;
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    self.currentIndex = indexPath;
     [self performSegueWithIdentifier:@"showFullscreen" sender:nil];
 }
 
+#pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     [self loadImagesForOnScreenItems];
 }
@@ -127,6 +120,61 @@ static NSString * const reuseIdentifier = @"GridCell";
     CGFloat ratio = photo.photoDimension.height / gridLayout.preferredHeight;
     
     return CGSizeMake(photo.photoDimension.width/ratio, photo.photoDimension.height/ratio);
+}
+
+#pragma mark - MGViewControllerDelegate
+- (void)viewController:(UIViewController *)viewController didUpdateToIndexPath:(NSIndexPath *)indexPath {
+    [self.collectionView scrollToItemAtIndexPath:indexPath atScrollPosition:UICollectionViewScrollPositionCenteredVertically animated:NO];
+    [self.collectionView reloadItemsAtIndexPaths:@[indexPath]];
+}
+
+- (void)viewControllerDidClose:(UIViewController *)viewController {
+    self.currentIndex = nil;
+}
+
+#pragma mark - Notifiers
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([[segue identifier] isEqualToString:@"showFullscreen"]) {
+        MGGalleryFullscreenCollectionViewController *fullscreenController = (MGGalleryFullscreenCollectionViewController *)[segue destinationViewController];
+        
+        fullscreenController.selectedIndexPath = self.currentIndex;
+        fullscreenController.photoArray = self.photoArray;
+        fullscreenController.pageNumer = self.pageNumer;
+        [fullscreenController setControllerDelegate:self];
+    }
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    NSIndexPath *midIndexPath = self.currentIndex;
+    
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+        [self.collectionView scrollToItemAtIndexPath:midIndexPath atScrollPosition:UICollectionViewScrollPositionCenteredVertically animated:NO];
+    }];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+#pragma mark - UINavigationControllerDelegate
+- (id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC {
+    return [[MGTransitionAnimator alloc] initWithOperation:operation];
+}
+
+#pragma mark - Helper Methods
+- (void)downloadUpdateContent {
+    self.pageNumer += 1;
+    [self.apiClient getListPhotosForFeature:kMG500pxPhotoFeaturePopular
+                         includedCategories:@[]
+                         excludedCategories:@[kMG500pxPhotoCategoryNude]
+                                       page:self.pageNumer
+                                 completion:^(NSArray *result, NSError *error) {
+                                     [self.photoArray addObjectsFromArray:[result copy]];
+                                     [self.collectionView reloadData];
+                                 }];
 }
 
 - (void)loadImageFor:(MGPhoto *)photo forCell:(MGGridCollectionViewCell *)gridCell atIndexPath:(NSIndexPath *)indexPath withCollectionView:(UICollectionView *)collectionView {
@@ -157,7 +205,7 @@ static NSString * const reuseIdentifier = @"GridCell";
         NSArray *visiblePaths = [self.collectionView indexPathsForVisibleItems];
         for (NSIndexPath *indexPath in visiblePaths) {
             MGPhoto *photo = self.photoArray[indexPath.item];
-            MGGridCollectionViewCell *cell = (id)[self.collectionView cellForItemAtIndexPath:indexPath];
+            MGGridCollectionViewCell *cell = (MGGridCollectionViewCell *)[self.collectionView cellForItemAtIndexPath:indexPath];
             if (photo.photoImage) {
                 [cell setImage:photo.photoImage];
                 photo.wasShown = YES;
@@ -166,36 +214,4 @@ static NSString * const reuseIdentifier = @"GridCell";
     }
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if ([[segue identifier] isEqualToString:@"showFullscreen"]) {
-        MGGalleryFullscreenCollectionViewController *fullscreenController = (MGGalleryFullscreenCollectionViewController *)[segue destinationViewController];
-        
-        fullscreenController.selectedIndexPath = self.currentSelectedIndex;
-        fullscreenController.photoArray = self.photoArray;
-        fullscreenController.pageNumer = self.pageNumer;
-        [fullscreenController setControllerDelegate:self];
-    }
-}
-
-#pragma mark - MGViewControllerDelegate
-- (void)viewController:(UIViewController *)viewController didUpdateToIndexPath:(NSIndexPath *)indexPath {
-    self.currentIndex = indexPath;
-    [self.collectionView scrollToItemAtIndexPath:self.currentIndex atScrollPosition:UICollectionViewScrollPositionCenteredVertically animated:NO];
-    [self.collectionView reloadItemsAtIndexPaths:@[self.currentIndex]];
-}
-
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-    
-    [self.collectionView scrollToItemAtIndexPath:self.currentIndex atScrollPosition:UICollectionViewScrollPositionCenteredVertically animated:NO];
-    [self.collectionView reloadItemsAtIndexPaths:@[self.currentIndex]];
-}
-
-- (BOOL)prefersStatusBarHidden {
-    return YES;
-}
-
-- (id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC {
-    return [[MGTransitionAnimator alloc] initWithOperation:operation];
-}
 @end
